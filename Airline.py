ac765870a6de50da2bf8cec7a15a5dd99515a4c4
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-09-04 18:32:03 +0200

    Update Airline.py

--- Airline.py
+++ Airline.py
@@ -3,11 +3,19 @@ class Airline:
         self.name = name
         self.flights,self.nf,self.nid_index_dict,self.index_nid_dict = self.getFlights()
         self.flights_copies = self.getFlightsCopies()
+        self.P = self.getP()
         self.itineraries,self.FI = self.getItineraries()
         self.IR = self.getIR()
+        self.routes = self.getRoutes()
+        self.F = self.flights.iloc[:,0]
+        self.R = self.getR()
+        self.IC = self.getIC()
+        
         self.Fare = self.getFare()
         self.B = self.getB()
         self.DI = self.getDI()
+        self.dev = self.get_dev()
+        
         
     def getFlights(self):
         airline_flights = flights[flights.airline==self.name]
@@ -57,6 +65,13 @@ class Airline:
             counter += 1
             flights_copies.append(list1)
         return flights_copies
+
+    def getP(self):
+        P = list()
+        for i in range(3):
+            for f in self.flights_copies:
+                P.append(f)
+        return P
     
     def getItineraries(self):
         G = nx.MultiDiGraph()
@@ -121,18 +136,154 @@ class Airline:
                     list1.append(j)
             IR.append(list1)
         return IR
+
+    def getIC(self):
+        IC = list()
+        for i in self.FI:
+            if (len(i)==2):
+                IC.append(i)
+        return IC
+
+    def getFM(self):
+        return self.flights[self.flights.mandatory == 1].iloc[:,0]
+
+    def getFO(self):
+        return self.flights[self.flights.mandatory == 0].iloc[:,0]
+
+    def getFIO(self):
+        FIO = list()
+        for i in range(len(self.FI)):
+            list1 = list()
+            for f in self.FI[i]:
+                if(self.flights.iloc[self.nid_index_dict[f],23] == 0):
+                    list1.append(f)
+            FIO.append(list1)
+        return FIO
+
+    def getA(self):
+        A = list()
+        for i in range(1,len(self.routes)+1):
+            if (i<10):
+                s = "00" + str(i)
+            if (i>= 10 and i<100):
+                s = "0" + str(i)
+            if (i>=100):
+                s = str(i)
+            A.append(self.name + s)
+        return A
+
+    def getR(self):
+        RA = list()
+        for r in self.routes:
+            list1 = list()
+            list1.append(list(r))
+            for f in r:
+                for p in self.flights_copies[self.nid_index_dict[f]]:
+                    list2 = list(r)
+                    list2 = self.substitution_in_list(list2,f,p)
+                    if (list2 != list(r)):
+                        list1.append(list2)
+            RA.append(list1)          
+        R = list()
+        counter = 0
+        for i in range(len(RA)):
+            list1 = list()
+            for j in range(len(RA[i])):
+                list1.append(counter)
+                counter +=1
+            R.append(list1)
+        return R
     
     def getFare(self):
         Fare = list()
         for i in self.FI:
             Fare.append(self.fare_for_itinerary(i))
         return Fare
+
+
+    def getD(self):
+        D = list()
+        for i in range(len(self.FI)):
+            D.append(self.find_min_pax(self.FI[i]))
+        return(D)
     
     def getDI(self):
         DI = list()
         for i in range(len(self.FI)):
             DI.append(self.find_min_cap(self.FI[i]))
         return DI
+
+    def getC(self):
+        C = list()
+        for i in range(len(self.R)):
+            C.append(self.calc_route_cost(self.R[i]))
+        return C
+
+    def getFT(self):
+        FT = list()
+        for i in range(len(self.F)):
+            dep = pd.to_datetime(self.flights['sobt']).iloc[i]
+            arr = pd.to_datetime(self.flights['sibt']).iloc[i]
+            time = arr - dep
+            FT.append(self.calc_minutes(time))
+        return FT
+
+    def getΘ1(self):
+        Θ1 = list()
+        for i in range(len(self.flights)):
+            list1 = list()
+            for p in self.P[i]:
+                list2 = list()
+                for j in range(len(self.R)):
+                    if (p in self.R[j]):
+                        list2.append(1)
+                    else:
+                        list2.append(0)
+                list1.append(list2)
+            Θ1.append(list1)
+        return Θ1
+
+    def getΘ2(self):
+        Θ2 = list()
+        for i in self.FI:
+            list1 = np.zeros(len(self.flights))
+            for f in i:
+                list1[self.nid_index_dict[f]] = 1
+            Θ2.append(list1)
+        return Θ2
+
+    def getΘ3(self):
+        Θ3 = list()
+        for i in self.FI:
+            list1 = list()
+            for j in range(len(self.R)):
+                if (all(flight in self.R[j] for flight in i)):
+                    list1.append(1)
+                else:
+                    list1.append(0)
+            Θ3.append(list1)
+        return Θ3
+
+    def getγ(self):
+        γ = list()
+        for i in self.FI:
+            list1 = list()
+            for f1 in i:
+                list2 = list()
+                for p1 in self.P[self.nid_index_dict[f1]]:
+                    list3 = list()
+                    for f2 in i:
+                        list4 = list()
+                        for p2 in self.P[self.nid_index_dict[f2]]:
+                            if (f1 == f2 or self.existICbetween(f1,p1,f2,p2)):
+                                list4.append(1)
+                            else:
+                                list4.append(0)
+                        list3.append(list4)
+                    list2.append(list3)
+                list1.append(list2)        
+            γ.append(list1)
+        return γ
     
     def getB(self):
         B = list()
@@ -152,7 +303,42 @@ class Airline:
                         rate_max = 0
             B.append(list1)
         return B
-    
+
+    def get_dev(self):
+        dev = list()
+        for r in range(len(self.R)):
+            list1 = list()
+            for f in range(len(self.F)):
+                if (self.P[f][0] in self.R[r]):
+                    list1.append(random.randint(1,10))
+                else:
+                    list1.append(0)
+            dev.append(list1)
+        return dev
+
+    def getRoutes(self):
+        flights_nid = list(self.flights['nid'])
+        G = nx.Graph()
+        for i in range(self.nf):
+            if (pd.notnull(self.flights.iloc[i,21]) and (self.flights.iloc[i,21] in flights_nid)):
+                G.add_edge(self.flights.iloc[i,21], self.flights.iloc[i,0])
+        aircraft_routes = list()
+        for i in nx.connected_components(G):
+            aircraft_routes.append(list(i))
+        return aircraft_routes
+
+    def getEPD(self):
+        EPD = list()
+        for r in range(len(self.R)):
+            list1 = list()
+            for f in range(len(self.R[r])):
+                delay_sum = 0
+                for d in range(f):
+                    delay_sum += self.dev[r][f]
+                list1.append(delay_sum)
+            EPD.append(list1)
+        return EPD
+        
     def find_flights_itineraries(self,itinerary):
         flights1 = self.flights[self.flights['origin']== itinerary[0]]
         flight1 = flights1[flights1['destination']== itinerary[1]].iloc[0,0]
@@ -186,3 +372,44 @@ class Airline:
                 and(self.itineraries[j].index(self.itineraries[i][a])<self.itineraries[j].index(dest))for a in range(len(self.itineraries[i])-1))):
             return True
         return False  
+
+    def find_min_pax(self):
+        minimum = np.Inf
+        for i in self.FI:
+            pax = self.flights[self.flights['nid']==i].iloc[0,18]
+            if (pax < minimum ):
+                minimum = pax
+        return minimum
+
+    def calc_route_cost(self,route):
+        cost = 0
+        for f in route:
+            cost += self.calc_flight_cost(self.flights.iloc[self.nid_index_dict[f],:])
+        return cost
+
+    def calc_flight_cost(self,flight):
+        dep = flight['sobt']
+        arr = flight['sibt']
+        dep = pd.to_datetime(str(dep))
+        arr = pd.to_datetime(str(arr))
+        return self.calc_minutes(arr-dep) * 2000/60
+    
+    def calc_minutes(self,date):
+        h = str(date)[7:9]
+        m = str(date)[10:12]
+        return int(m)+int(h)*60
+
+    def existICbetween(f1,p1,f2,p2):
+        for i in self.IC:
+            if (((i[0] == f1)or(i[0]==p1)) and ((i[1] == f2)or(i[1] == p2))):
+                return True
+        return False
+
+    def substitution_in_list(self,r,f,p):
+        index = len(r)+1
+        for i in range(len(r)):
+            if (r[i] == f):
+                index = i
+                break
+        r[index] = p
+        return r
