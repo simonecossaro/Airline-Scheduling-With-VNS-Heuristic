d420ead753535276c43f1d79d2a8c2429c51cd22
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-09-04 16:51:21 +0200

    Update medium_airline_vns.py

--- Medium Airline Models/medium_airline_vns.py
+++ Medium Airline Models/medium_airline_vns.py
@@ -1,1 +1,13 @@
+import time
+import Airline
+import VNS
 
+start = time.perf_counter()
+medium_airline = Airline('ISS')
+vns = VNS(medium_airline)
+sol, obj_value = vns.search(10)
+end = time.perf_counter()
+print('Solution aircraft routing problem: ')
+print(sol)
+print('Revenue(objective function value): '+ str(obj_value))
+print('Task time: '+str(ma_end-ma_start)+ ' s')
