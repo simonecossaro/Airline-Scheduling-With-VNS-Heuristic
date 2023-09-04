c83018b48d233bef623596aa5513db5dc4076fcf
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-09-04 15:32:54 +0200

    Create FeasibleSolutionSearcher.py

---
+++ VNS/FeasibleSolutionSearcher.py
@@ -0,0 +1,57 @@
+class FeasibleSolutionSearcher:
+    def __init__(self,airline):
+        self.airline = airline
+        self.routes_graph = self.createRoutesGraph()
+        
+    def search(self):
+        D = self.routes_graph
+        edge_index_dict = dict()
+        index_edge_dict = dict()
+        D_edges = list()
+        counter = 0
+        for e in D.edges():
+            D_edges.append(list([e[0],e[1]]))
+            edge_index_dict[e] = counter
+            index_edge_dict[counter] = e
+            counter +=1
+        A0 = self.createA0(D)
+        ip = gb.Model()
+        ip.Params.LogToConsole = 0
+        X = ip.addVars( [(a) for a in range(len(D.edges()))], vtype=gb.GRB.BINARY)
+        # (1)
+        for v in D.nodes():
+            if (D.in_degree(v) != 0 and D.out_degree(v) != 0 ):
+                ip.addConstr( gb.quicksum(X[edge_index_dict[(a,v)]] for a in D.predecessors(v)) == gb.quicksum(X[edge_index_dict[(v,a)]] for a in D.successors(v)) )
+        # (2)
+        for v in D.nodes():
+            if (D.in_degree(v) != 0 ):
+                ip.addConstr( gb.quicksum(X[edge_index_dict[(a,v)]] for a in  D.predecessors(v)) == 1)
+        # (3)
+        ip.addConstr( gb.quicksum(X[edge_index_dict[a]] for a in A0) <= len(D_edges) )
+        f_sol = ip.optimize()
+
+        D2 = nx.Graph()
+        for i in range(len(X)):
+            if(X[i].x==1):
+                D2.add_edge(D_edges[i][0],D_edges[i][1])
+        feasible_solution = list()
+        for i in nx.connected_components(D2):
+            feasible_solution.append(list(i))
+        return feasible_solution
+    
+    def createA0(self,D):
+        instant = pd.to_datetime('2014-09-12 '+str(23)+':05:00')
+        A0 = list()
+        for e in D.edges():
+            if((pd.to_datetime(self.airline.flights[self.airline.flights.nid == e[0]].iloc[0,11]) <= instant) 
+               and (pd.to_datetime(self.airline.flights[self.airline.flights.nid == e[1]].iloc[0,11]) > instant)):
+                A0.append(e)
+        return A0
+    
+    def createRoutesGraph(self):
+        D = nx.DiGraph()
+        for i in range(len(self.airline.routes)):
+            for j in range(len(self.airline.routes[i])-1):
+                D.add_edge(self.airline.routes[i][j],self.airline.routes[i][j+1])
+        return D
+        
