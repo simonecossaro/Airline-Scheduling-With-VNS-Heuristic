02eddc45b21f94de8739bd5566d0a5ff67fdaaeb
simone <simonecossaro@gmail.com>
2023-09-13 12:32:15 +0200

    last updates

--- Small Airline Models/model111.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-results = pd.read_csv('results.csv').iloc[:,1:]
-
-sum = 0
-for i in range(len(results)):
-    sum += (1 - results.iloc[i,5]/results.iloc[i,4])
-
-print(sum / len(results))
